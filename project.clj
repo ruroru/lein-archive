1dc8fcf41714674c7f3976b878a9c7e3cc5dcbc8
ruroru <111705692+ruroru@users.noreply.github.com>
2025-07-31 07:01:22 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/archive "1.1.2"
+(defproject org.clojars.jj/archive "1.1.3-SNAPSHOT"
   :description "Leiningen plugin to effortlessly create archives of your project files. Supports tgz and zip formats"
   :url "https://github.com/ruroru/lein-archive"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
